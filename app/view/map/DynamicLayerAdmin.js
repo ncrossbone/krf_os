07d3cf91871e7502bde510a4e88d22992986aa0b
pdj <ncrossbone@gmail.com>
2018-03-20 09:23:36 +0900

    commit - 레이어 번호 변경

--- app/view/map/DynamicLayerAdmin.js
+++ app/view/map/DynamicLayerAdmin.js
@@ -17,7 +17,7 @@ Ext.define('krf_new.view.map.DynamicLayerAdmin', {
 		me.dynamicLayer1.visible = true;
 		//		me.dynamicLayer1.showAttribution = true;
 
-		me.dynamicLayer1.setVisibleLayers([1, 2, 3, 4, 5, 76, 77, 53, 54, 55, 62, 63]);
+		me.dynamicLayer1.setVisibleLayers([1, 2, 3, 4, 5, 79, 80, 56, 57, 58, 65, 66]);
 
 		me.map.addLayer(me.dynamicLayer1);
 		me.dynamicLayer2 = new esri.layers.ArcGISDynamicMapServiceLayer($KRF_DEFINE.reachServiceUrl_v3);
