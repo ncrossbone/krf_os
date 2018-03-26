2b7cdebc079b7d2efd4d36e8883764f70629dad3
hyeokpark <hyeokpark92@gmail.com>
2018-03-26 16:19:04 +0900

    탭 수정

--- build/temp/production/krf_new/sencha-compiler/app/full-page-master-bundle.js
+++ build/temp/production/krf_new/sencha-compiler/app/full-page-master-bundle.js
@@ -1,6 +1,2 @@
 // @tag full-page
-<<<<<<< HEAD
 // @require F:\PARKHYEOK\ALL_WORKSPACES\KRF_WORKSPACE\krf_os\app.js
-=======
-// @require C:\git\KRF_OS\app.js
->>>>>>> 50be611a2cef7295c9d12fe2ce04d064d12c0203
