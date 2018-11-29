89006d4656f08d81cdb506f4f131fd6c7ac1ba89
pdj <djp5014@nate.com>
2018-11-29 15:15:14 +0900

    내부망 보 COMMIT

---
+++ app/view/east/HighChartPanelController.js
@@ -0,0 +1,9 @@
+/**
+* North Controller
+*/
+Ext.define('krf_new.view.east.HighChartPanelController', {
+	extend: 'Ext.app.ViewController',
+
+	alias: 'controller.highChartPanelController',
+	id: 'highChartController',
+});
